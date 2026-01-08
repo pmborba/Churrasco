324696d182e1eecc714d46bc8c4983c0109a29de
pmborba <pborba91@gmail.com>
2026-01-08 16:52:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.markdown(
 
     /* CAMPOS DE ENTRADA: 70% Transparentes */
     .stNumberInput div div {{
-        background-color: rgba(255, 255, 255, 0.7) !important;
+        background-color: rgba(255, 255, 255, 0.3) !important;
         color: black !important;
         border-radius: 10px;
     }}
